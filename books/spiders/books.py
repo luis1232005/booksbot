65c202d17ebcc05ab7a90f3fe6aa635081fbf8cb
luis liu <514105724@qq.com>
2019-06-12 17:12:44 +0800

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -4,27 +4,20 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "books"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["xkm.com.tw"]
     start_urls = [
-        'http://books.toscrape.com/',
+        "http://www1.xkm.com.tw/hr/DATA/HR190604.htm"
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
-
-    def parse_book_page(self, response):
-        item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
+        count = 0
+        for tr in response.xpath('//body/table/tr'):
+            count = count + 1
+            tds = len(tr.xpath('td'))
+            item = {}
+            if(count > 4 and tds > 6):
+                item['tv'] = tr.xpath('td[2]/text()').extract()
+                item['program'] = tr.xpath('td[3]/text()').extract()
+                item['time'] = tr.xpath('td[4]/text()').extract()
+                item['viewingRate'] = tr.xpath('td[5]/text()').extract()
+                yield item
